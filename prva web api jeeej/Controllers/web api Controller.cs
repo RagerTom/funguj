1c68dcb1eac2dbc9a15b1b8844bc38c70e4113a5
RagerTom <tomas.moravcik04@gmail.com>
2022-04-25 19:24:46 +0200

    Add project files.

---
+++ prva web api jeeej/Controllers/web api Controller.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace prva_web_api_jeeej.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class web_api_Controller : ControllerBase
+    {
+
+        private static List<web_api> ziak = new List<web_api>
+            {
+                new web_api {
+                    Id = 1,
+                    FirstName = "Janko",
+                    LastName = "Mrkvicka",
+                    Age = 20
+                }
+            };
+        [HttpGet]
+        [Route("ziaci")]
+        public async Task<ActionResult<List<web_api>>> Get()
+        {
+
+
+            return Ok(ziak);
+        }
+
+        [HttpGet]
+        
+        public async Task<ActionResult<List<web_api>>> addZiak(int id, string firstName, string lastName, int age)
+        {
+            web_api api = new web_api();
+
+            api.Id = id;
+            api.FirstName = firstName;
+            api.LastName = lastName;
+            api.Age = age;
+
+            ziak.Add(api);
+            return Ok(ziak);
+        }
+    }
+
+}
---
+++ prva web api jeeej/WeatherForecast.cs
@@ -0,0 +1,15 @@
+namespace prva_web_api_jeeej
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+
+
+    }
+}
